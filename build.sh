4d56e6bb35ede53688ea59910a9eb6b78f1f3367
NabOuat <ouatnaba1@gmail.com>
2025-10-07 18:17:48 +0000

    commit 2

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
+pip install --upgrade pip setuptools wheel
 
-# Install Python dependencies
+echo "=== Upgrade pip, setuptools et wheel ==="
+pip install --upgrade pip setuptools wheel
+
+echo "=== Installation des dépendances Python ==="
 pip install -r requirements.txt
 
-# Collect static files
+echo "=== Collecte des fichiers statiques ==="
 python manage.py collectstatic --no-input
 
-# Run migrations
+echo "=== Appliquer les migrations ==="
 python manage.py migrate
+
+echo "=== Build terminé ✅ ==="
