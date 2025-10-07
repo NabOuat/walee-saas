bd185b794f624960dd7f6feb20eb48a0ae4b75ce
NabOuat <ouatnaba1@gmail.com>
2025-10-07 18:20:55 +0000

    commit 3

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
-pip install --upgrade pip setuptools wheel
 
 echo "=== Upgrade pip, setuptools et wheel ==="
 pip install --upgrade pip setuptools wheel
@@ -12,7 +11,8 @@ pip install -r requirements.txt
 echo "=== Collecte des fichiers statiques ==="
 python manage.py collectstatic --no-input
 
-echo "=== Appliquer les migrations ==="
-python manage.py migrate
+# Si vous avez une DB prête, décommentez la ligne suivante
+# echo "=== Appliquer les migrations ==="
+# python manage.py migrate
 
 echo "=== Build terminé ✅ ==="
