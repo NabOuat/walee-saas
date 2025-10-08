e3515035fb47ac10c67eaaf074fcfcb2c32954e7
NabOuat <ouatnaba1@gmail.com>
2025-10-08 08:24:21 +0000

    Ready to Render

--- walee/settings.py
+++ walee/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-woi2$5#q%2#otrd)z=i5%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
 
 
 # Application definition
