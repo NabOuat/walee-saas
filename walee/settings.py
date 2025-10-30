a3b1c4c18af3f7f8ff26441501f2e0df354b1959
@nabaga.ouattarai <nabaga.ouattara@afor.ci>
2025-10-30 07:56:54 +0000

    Suppression du backend et configuration AUTH_USER_MODEL

--- walee/settings.py
+++ walee/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     
     # Local apps
+    "accounts",
     "frontend",
 ]
 
@@ -167,3 +168,6 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom User Model
+AUTH_USER_MODEL = 'accounts.Utilisateur'
